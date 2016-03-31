7b8e7dadbc18bcf3080b6c0969fa0b6d5318f0e7
Peter Springer <peterspringer829@gmail.com>
2016-03-30 22:29:36 -0600

    iteration 6

--- jungle_beats.rb
+++ jungle_beats.rb
@@ -1,4 +1,33 @@
+require "./linked_list.rb"
 class JungleBeats
+
+def initialize
+  @list = LinkedList.new
+end
+
+def list
+  @list
+end
+
+def append(data)
+  beats = data.split(" ")
+  beats.each do |beat|
+    @list.append(beat)
+  end
+  data
+end
+
+def count
+@list.count
+end
+
+def play
+  beats = @list.to_string
+  `say -r 500 -v Boing #{beats}`
+end
+
+
+
 
 
 
